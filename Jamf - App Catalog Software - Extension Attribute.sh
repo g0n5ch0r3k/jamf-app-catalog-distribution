7bd7a77fcf72a86100d5cd8b14c67d2e0d34e24f
g0n5ch0r3k <54019004+g0n5ch0r3k@users.noreply.github.com>
2022-11-09 11:08:17 +0100

    Update Jamf - App Catalog Software - Extension Attribute.sh
    added systemtype detection
    --> using defaults read on intel mac and plistbuddy on arm

--- Jamf - App Catalog Software - Extension Attribute.sh
+++ Jamf - App Catalog Software - Extension Attribute.sh
@@ -16,11 +16,18 @@ plist="jamf.catalogsoftware.distribution.plist"
 #Preferences path
 plistpath="/Library/Preferences/"
 
-#apparray=$($plistbuddy "$plistpath$plist" -c "Print :applications"| grep Dict | sed 's/^\(.*\) =.*/\1/g' | grep -v "Dict {")
-apparray=$($plistbuddy "$plistpath$plist" -c "Print :applications:array" | sed 's/^\(.*\) =.*/\1/g' | grep -v "Dict {" | grep -v "}")
-result=$(for app in ${apparray[@]}; do 
+systemtype=$(uname -m)
+echo "Systemtype: $systemtype"
+if [[ $systemtype == 'arm64' ]]; then
+
+	apparray=$($plistbuddy "$plistpath$plist" -c "Print :applications:array" | sed 's/^\(.*\) =.*/\1/g' | grep -v "Dict {" | grep -v "}")
+	result=$(for app in ${apparray[@]}; do 
 	plistapprequested=$($plistbuddy -c "print :applications:array:$app" $plistpath$plist || printf '0')
 	appname=${app//_/ }
 	echo "$appname = $plistapprequested"; 
 done)
+else
+	result=$(defaults read /Library/Preferences/jamf.catalogsoftware.distribution.plist applications | grep -v "{\|}\|(\|)")
+fi
+
 echo "<result>$result</result>"
